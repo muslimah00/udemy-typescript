c9ce2141f807ed72703c249d505d175af9023f0d
limaxhy5 <muslimah2128@gmail.com>
2026-06-03 11:06:10 +0800

    catching error with ts

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
-  // Pretty-print JSON with 2-space indentation
-  const todo = response.data;
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.fisinhed;
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
 
   console.log(`
     The todo with id: ${ID}
