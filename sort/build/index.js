f2616701d8daebe58851fa36f9ca884fbb0bf812
Lima <muslimah2128@gmail.com>
2026-07-05 08:36:13 +0800

    linkedlist

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,10 +1,18 @@
 import { Sorter } from './Sorter.js';
-import { CharactersCollection } from './CharactersCollection.js';
+import { LinkedList } from './LinkedList.js';
 // const numbersCollection = new NumbersCollection ([20, 4, -5, -6]);
 // const sorter = new Sorter (numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data)
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import {Sorter} from './Sorter.js';
 import {NumbersCollection} from './NumbersCollection.js';
 import { CharactersCollection } from './CharactersCollection.js';
+import { LinkedList } from './LinkedList.js';
 
 // const numbersCollection = new NumbersCollection ([20, 4, -5, -6]);
 // const sorter = new Sorter (numbersCollection);
@@ -9,7 +10,17 @@ import { CharactersCollection } from './CharactersCollection.js';
 
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data)
+linkedList.print();
