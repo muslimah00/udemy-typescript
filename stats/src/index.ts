792ddefc5b84fd697b34b9d94ca9480021d25755
limaxhy5 <muslimah2128@gmail.com>
2026-07-13 11:06:36 +0800

    generate report html

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,9 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-<<<<<<< HEAD
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-=======
-import { MatchResult } from './MatchResult';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -17,28 +12,7 @@ const csvFileReader = new CsvFileReader('football.csv');
 // the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
->>>>>>> ac5c7e29684a7b444a376e0f39b31ebcb99e50e7
 
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
-<<<<<<< HEAD
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
-=======
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
->>>>>>> ac5c7e29684a7b444a376e0f39b31ebcb99e50e7
