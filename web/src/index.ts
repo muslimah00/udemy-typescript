fef56d0acb5e7fb6a5e031228daa6565bc270e0a
Lima <muslimah2128@gmail.com>
2026-07-14 18:36:27 +0800

    feat: update User model to include save functionality and modify user instantiation in index.ts

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'New Lima', age: 20 });
 
-user.fetch();
 
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+user.save();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -44,6 +44,16 @@ export class User {
       .get(`http://localhost:3000/users/${this.get('id')}`)
       .then((response: AxiosResponse): void => {
         this.set(response.data);
-      });
+      }); 
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    } else {
+      axios.post('http://localhost:3000/users', this.data)
+    }
   }
 }
